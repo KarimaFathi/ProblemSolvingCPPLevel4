7d14b3a29e1795239ae945347aa9c5f8489b7990
KarimaFathi <karimafathifadili@gmail.com>
2025-02-20 23:58:57 +0100

    Update Exo10

--- Exo10/Exo10.cpp
+++ Exo10/Exo10.cpp
@@ -23,19 +23,24 @@ short NumberOfDaysInAMonth(short year, short month) {
     return (month == 4 || month == 6 || month == 9 || month == 11) ? 30 : (month == 2) ? (isLeapYear(year) ? 29 : 28) : 31;
 }
 
+
+short NumberOfDaysFromTheBeginningOfTheYear(short year, short month, short day) {
+    short result = 0;
+    for (short i = 1; i < month; i++) {
+        result += NumberOfDaysInAMonth(year, i);
+    }
+    result += day;
+    return result;
+}
+
 int main()
 {
 
     short year = readNumber("Please enter a year : ");
     short month = readNumber("\nPlease enter a month : ");
     short day = readNumber("\nPlease enter a day : ");
-    short result = 0;
-
-    for (short i = 1; i < month; i++) {
-        result += NumberOfDaysInAMonth(year, i);
-    }
-    result += day;
-    cout << result;
+  
+    cout << "\nNumber of Days from the begining of the year is " << NumberOfDaysFromTheBeginningOfTheYear(year, month, day);
 
 }
 
